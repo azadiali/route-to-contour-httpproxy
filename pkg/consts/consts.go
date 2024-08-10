9512fcc130214a3d9d901dc0c7a8a90d03559967
ali.azadi <ali.azadi@snapp.cab>
2024-08-10 15:27:51 +0330

    Fix: indentation of consts.go

--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -1,29 +1,32 @@
 package consts
+
 import (
-    "os"
+	"os"
 	"strconv"
 )
+
 type CustomError string
 
 func (e CustomError) Error() string { return string(e) }
 
-
 var (
-    IngressClassPrivate = getEnv("INGRESS_CLASS_PRIVATE", "private")
-	IngressClassPublic = getEnv("INGRESS_CLASS_PUBLIC", "public")
-    IngressClassInterDc = getEnv("INGRESS_CLASS_INTER_DC", "inter-dc")
-	IngressClassDefault = getEnv("INGRESS_CLASS_DEFAULT", "private")
-	TLSSecretNS = getEnv("TLS_SECRET_NS", "openshift-ingress")
-	TLSSecretName = getEnv("TLS_SECRET_NAME","letsencrypt")
-	GlobalTLSSecretName = TLSSecretNS + "/" + TLSSecretName
-    EnableFallbackCertificate, _ = strconv.ParseBool(getEnv("ENABLE_FALLBACK_CERTIFICATE", "true")))
+	IngressClassPrivate          = getEnv("INGRESS_CLASS_PRIVATE", "private")
+	IngressClassPublic           = getEnv("INGRESS_CLASS_PUBLIC", "public")
+	IngressClassInterDc          = getEnv("INGRESS_CLASS_INTER_DC", "inter-dc")
+	IngressClassDefault          = getEnv("INGRESS_CLASS_DEFAULT", "private")
+	TLSSecretNS                  = getEnv("TLS_SECRET_NS", "openshift-ingress")
+	TLSSecretName                = getEnv("TLS_SECRET_NAME", "letsencrypt")
+	GlobalTLSSecretName          = TLSSecretNS + "/" + TLSSecretName
+	EnableFallbackCertificate, _ = strconv.ParseBool(getEnv("ENABLE_FALLBACK_CERTIFICATE", "true"))
+)
 
 func getEnv(key, fallback string) string {
-    if value, exists := os.LookupEnv(key); exists {
-        return value
-    }
-    return fallback
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
 }
+
 const (
 	LabelKeyRouterName = "router"
 
